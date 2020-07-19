a02a091e54a0ddbc0adbdb4319c61776712c48f4
sudo-dax <dax.scholtz@gmail.com>
2020-07-19 13:47:28 +1000

    CRUD operations added

---
+++ controllers/events_controller.js
@@ -0,0 +1,65 @@
+const {getAllEvents, getEventById, addEvent, deleteEvent, updateEvent} = require("../utils/events_utilities")
+const { restart } = require("nodemon")
+
+const getEvents = function (req, res) {
+    getAllEvents().exec((err, events) => {
+        if (err) {
+            res.status(500)
+        return res.json({
+            error: err.message
+        })
+        }
+        res.send(events)
+    })
+}
+
+const getEvent = function (req, res) {
+    getEventById(req.params.id).exec((err, event) => {
+        if (err) {
+            res.status(404)
+            return res.send("Event not found!")
+        }
+        res.send(event)
+    })
+}
+
+const postEvent = function (req, res) {
+    // Save the Event Instance from addEvent
+    addEvent(req.body).save((err, event) => {
+        if (err) {
+            res.status(500)
+            return res.json({
+                error: err.message
+            })
+        }
+        res.status(201)
+        res.send(event)
+    })
+}
+
+const removeEvent = function (req, res) {
+    deleteEvent(req.params.id).exec((err) => {
+        if (err) {
+            res.status(500)
+            return res.json({
+                error: err.message
+            })
+        }
+        res.sendStatus(204)
+    })
+}
+
+const changeEvent = function (req, res) {
+    updateEvent(req).exec((err, event) => {
+        if (err) {
+            res.status(500)
+            return res.json ({
+                error: err.message
+            })
+        }
+        res.status(200)
+        res.send(event)
+    })
+}
+
+module.exports = {getEvents, getEvent, postEvent, removeEvent, changeEvent}
---
+++ routes/events_routes.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const router = express.Router()
+const {getEvents, getEvent, postEvent, removeEvent, changeEvent} = require("../controllers/events_controller")
+
+
+// READ
+// GET on "/events"
+// Returns all events
+router.get("/", getEvents)
+
+// READ
+// GET on "/events/:id"
+// Returns an event with given id
+router.get ("/:id", getEvent)
+
+// CREATE
+// POST in "/events"
+// Creates a new event
+router.post("/", postEvent)
+
+// DELETE
+// DELETE in "/events/:id"
+// Deletes an event with given id
+router.delete("/:id", removeEvent)
+
+// UPDATE
+// PUT on "/events/:id"
+// Updates an event with given id
+router.put("/:id", changeEvent)
+
+module.exports = router;
